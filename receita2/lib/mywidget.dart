dfbf072bbe541010cadc2844a1487bccf3280131
gcileno <gabriel.cileno.093@ufrn.com.brm>
2023-04-24 20:07:51 -0300

    tarefa 6  - formulario basicos

--- receita2/lib/mywidget.dart
+++ receita2/lib/mywidget.dart
@@ -40,6 +40,92 @@ class MostrarDados extends StatelessWidget {
   }
 }
 
+class Myform extends StatelessWidget {
+  Myform();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 16),
+          Text('Escolha um valor'),
+          SizedBox(height: 16),
+          Slider(
+            value: 0.0,
+            min: 0.0,
+            max: 50,
+            divisions: 10,
+            label: 'Valor: 0.0',
+            onChanged: (double value) {},
+          ),
+          SizedBox(height: 16),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Digite seu nome',
+            ),
+          ),
+          SizedBox(height: 16),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              hintText: 'Digite sua senha',
+            ),
+          ),
+          SizedBox(
+            height: 16,
+          ),
+          TextField(
+              decoration: InputDecoration(hintText: "Escreva sua mensagem")),
+          SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              final snackbar = SnackBar(
+                content: Text('Dados cadastrados'),
+                duration: Duration(seconds: 2),
+              );
+              ScaffoldMessenger.of(context).showSnackBar(snackbar);
+            },
+            child: Text('Enviar'),
+          ),
+          Row(
+            children: <Widget>[
+              Switch(
+                value: false,
+                onChanged: (value) {},
+              ),
+              Switch(
+                value: true,
+                onChanged: (value) {},
+              ),
+              Switch(
+                value: false,
+                onChanged: (value) {},
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Checkbox(value: false, onChanged: (value) => {}),
+              Text('Café'),
+              SizedBox(
+                height: 16,
+              ),
+              Checkbox(value: false, onChanged: (bool) => {}),
+              Text('Paises'),
+              SizedBox(
+                height: 16,
+              ),
+              Checkbox(value: false, onChanged: (value) => {}),
+              Text('Cervejas'),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
 class Corpo extends StatelessWidget {
   List<Map<String, dynamic>> objetos;
   Corpo({this.objetos = const []});
